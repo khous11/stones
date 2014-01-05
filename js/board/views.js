54018951a5a086c74e05e35319f9d4e0f57630ed
Jason Housley <housleyjk@gmail.com>
2014-01-05 14:20:27 +0900

    Removing bonsai code.

--- js/board/views.js
+++ js/board/views.js
@@ -5,40 +5,11 @@
   v.BoardView = Backbone.View.extend({
     
     initialize: function(args) {
-        this.movie = args.movie;
-        this.testme = "hi";
     },
     
     
     render: function() {
-      this.movie.sendMessage('delegate', this.drawBoard());
     },
-    
-    drawBoard: function() {
-      var again = this.testme;
-      return function(stage) {
-         new Text(this.testme).addTo(stage).attr({
-              fontFamily: 'Arial, sans-serif',
-              fontSize: '20',
-              textFillColor: 'red',
-              textStrokeColor: 'yellow',
-              textStrokeWidth: 3
-            });
-            
-         new Text(again).addTo(stage).attr({
-              fontFamily: 'Arial, sans-serif',
-              fontSize: '36',
-              textFillColor: 'blue',
-              textStrokeColor: 'yellow',
-              textStrokeWidth: 3,
-              x: 80
-            });
-        
-      }
-      
-    }
-    
-    
     
   });
   
